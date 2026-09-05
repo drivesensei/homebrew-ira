cbdbe7e7dc33af04bd7a5ae1bff6fe0674d4c67c
drivesensei <drivesenseimx@gmail.com>
2026-09-05 00:54:35 -0600

    Fix formula download URL (asset filenames have no v prefix)

--- Formula/ira.rb
+++ Formula/ira.rb
@@ -1,7 +1,7 @@
 class Ira < Formula
   desc "Terminal file manager built with Ratatui and crossterm"
   homepage "https://github.com/drivesensei/ira"
-  url "https://github.com/drivesensei/ira/releases/download/v0.1.0-rc.3/ira-v0.1.0-rc.3-universal-apple-darwin.tar.xz"
+  url "https://github.com/drivesensei/ira/releases/download/v0.1.0-rc.3/ira-0.1.0-rc.3-universal-apple-darwin.tar.xz"
   sha256 "629fe37226b8aa7539b0a5c7eb31c17ddcdf0c7e964b69422df0b87e016a5b23"
   license "MIT"
 
