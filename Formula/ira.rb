be0641858838e75641afef6da85634c612824ad7
drivesensei <drivesenseimx@gmail.com>
2026-09-18 19:28:51 +0000

    ira 0.1.18

--- Formula/ira.rb
+++ Formula/ira.rb
@@ -1,8 +1,8 @@
 class Ira < Formula
   desc "Terminal file manager built with Ratatui and crossterm"
   homepage "https://github.com/drivesensei/ira"
-  url "https://github.com/drivesensei/ira/releases/download/v0.1.17/ira-0.1.17-universal-apple-darwin.tar.xz"
-  sha256 "fac18cbcd1b06fffce52efb3a3c86be42c59d0ba418141bdfb424a454c52393d"
+  url "https://github.com/drivesensei/ira/releases/download/v0.1.18/ira-0.1.18-universal-apple-darwin.tar.xz"
+  sha256 "efeb6bd94f7896bee1fed96ebb03eb957a7926fdc9102d19617b101ecc1b39b8"
   license "MIT"
 
   def install
